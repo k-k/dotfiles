ec0fb0f4b21fee4dfa38698effbed24bd890edee
keith kirk <kkirk@undergroundelephant.com>
2013-12-04 08:49:01 -0800

    updated install.sh to remove .vim directory if present

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cp ./.bash_git ../
-cp ./.bash_profile ../
-cp ./.bashrc ../
-cp ./.gitconfig ../
-cp ./.gitignore_global ../
-cp ./.vimrc ../
-cp -r ./.vim/ ../
+cp .bash_git ~/
+cp .bash_profile ~/
+cp .bashrc ~/
+cp .gitconfig ~/
+cp .gitignore_global ~/
+cp .vimrc ~/
+
+if [ -e ~/.vim ]; then
+    sudo rm -r ~/.vim
+fi
+cp -r .vim ~/.vim
