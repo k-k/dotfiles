43bca159e4adea30f0db6857a8997b3dd712e5ca
keith kirk <kkirk@undergroundelephant.com>
2013-12-16 15:48:50 -0800

    fixed install script. now asks for user input to set gitconfig, as a simple convenience

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-cp .bash_git ~/
-cp .bash_profile ~/
-cp .bashrc ~/
-cp .gitconfig ~/
-cp .gitignore_global ~/
-cp .vimrc ~/
+bold=`tput bold`
+normal=`tput sgr0`
 
+# Remove any existing .vim folder
 if [ -e ~/.vim ]; then
     sudo rm -r ~/.vim
 fi
-cp -r .vim ~/.vim
+
+# Copt the required files to the home directory
+cp -r .bash_git .bash_profile .bashrc .gitconfig .gitignore_global .vimrc .vim git-completion.bash ~/
+
+# Setup .gitconfig
+echo "${bold}Let's configure your Git configuration!${normal}"$'\n'
+echo "What is your full name? (ex: John Doe)"
+read name
+echo "And what is your email address? (ex: test@gmail.com)"
+read email
+
+echo "[user]"$'\n\t'"email = $email"$'\n\t'"name = $name"$'\n' >> ~/.gitconfig
+
+echo $'\n'"** Finished!"
