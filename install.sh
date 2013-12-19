c800976d9194d6a5c4774f6e4023a066e17c3876
Keith Kirk <kkirk@undergroundelephant.com>
2013-12-19 10:49:16 -0800

    updated install script to also run update_syntax.php and added more output

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 bold=`tput bold`
 normal=`tput sgr0`
 
+echo "${bold}Setting up Dot Files for this machine!${normal}"$'\n'
+
+# Update PHP Syntax for this machine
+php vim-php-syntax/update_syntax.php
+echo "PHP Syntax updated based on installed packages..."
+
 # Remove any existing .vim folder
 if [ -e ~/.vim ]; then
     sudo rm -r ~/.vim
@@ -10,6 +16,8 @@ fi
 
 # Copy the required files to the home directory
 cp -r .bash_git .bash_profile .bashrc .gitconfig .gitignore_global .vimrc .vim git-completion.bash ~/
+echo "Dot Files copied into your home directory..."
+echo "Done."$'\n\n'
 
 # Setup .gitconfig
 echo "${bold}Let's configure your Git configuration!${normal}"$'\n'
